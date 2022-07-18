a73f8d7712f088a30592d8365fa98e196e825e76
Bruna Maria <bruna.mariap@outlook.com>
2022-07-17 22:39:43 -0300

    método de Petrick

--- metodo_petrick.py
+++ metodo_petrick.py
@@ -7,6 +7,7 @@ def termos_petrick(implicantes_primos_incobertos: dict, implicantes_primos_essen
     alfabeto = list(string.ascii_uppercase[4:])
     chaves = [x for x in implicantes_primos_incobertos]
     valores = [y for y in implicantes_primos_incobertos.values()]
+    letras_equacao = []
     print('valores', valores)
     for i in range(len(chaves)):
         termos[(f'{alfabeto[i]}', chaves[i])] = valores[i]
@@ -21,5 +22,28 @@ def termos_petrick(implicantes_primos_incobertos: dict, implicantes_primos_essen
                 for mintermo_2 in implicantes_primos_incobertos[chaves[j]]:
                     #print('termo 2',termo_2)
                     if mintermo_1 == mintermo_2:
-                        equacao += '(' + alfabeto[i] + " + " + alfabeto[j] + ').'
-    print(equacao[-1])
+                        equacao += '(' + alfabeto[i] + "+" + alfabeto[j] + ').'
+                        letras_equacao.append([alfabeto[i], alfabeto[j]])
+    print(equacao[:-1])
+    print(letras_equacao)
+    print(distributiva(equacao[:-1], letras_equacao))
+
+
+def distributiva(equacao, letras_equacao):
+    equacao = equacao.split('.')
+    print(equacao)
+    nova_equacao = []
+    nova_equacao2 = ''
+    for i in range(len(equacao)):
+        nova_equacao.append(list(equacao[i]))
+    
+    """ for i in range(0,len(letras_equacao) - 1):
+
+        for j in range(i + 1, len(letras_equacao)):
+            for letra in letras_equacao[j]:
+                print(letra)
+                if letras_equacao[i] == letra:
+                    nova_equacao2 += '(' + letras_equacao[i] + '+' + letras_equacao[i + 1] + letra + ')' """
+
+    print(nova_equacao2)
+    print(nova_equacao)
